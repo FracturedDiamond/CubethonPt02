bda2c41cf58ccdac8ab906c53797b3c2e3904ef5
FracturedDiamond <hpfan1516@gmail.com>
2020-01-19 20:46:54 -0700

    Initial

---
+++ Cubethon_Part2/Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool gameHasEnded = false;                  // Initialize gameHasEnded to 'false'
+    public float restartDelay = 1f;             // Initialize restartDelay to '1f'
+    public GameObject completeLevelUI;          // Declare completeLevelUI gameObject
+
+
+    public void CompleteLevel ()                // Function for completing a level
+    {
+        completeLevelUI.SetActive(true);        // Allow completeLevelUI to be seen
+    }
+
+
+
+
+    public void EndGame ()                      // Function to end the game -- public so other scripts can see
+    {
+        if (gameHasEnded == false)              // Check to see if game has ended yet
+        {
+            gameHasEnded = true;                // set gameHasEnded to 'true'
+
+            Invoke("Restart", restartDelay);    // Restart the game
+        }
+    }
+
+    void Restart ()                             // Function to restart the game
+    {
+        // Reload the active scene (i.e. if you are on level 02 it will restart level 02)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
---
+++ Cubethon_Part2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;                    // Declaring the RigidBody variable 'rb'
+
+    public float forwardForce = 1000f;      // Variable for amount of forward force
+    public float sidewaysForce = 500f;      // Variable for amount of sideways force
+    public float jumpForce = 500f;
+
+
+    void FixedUpdate ()                     // Using FixedUpdate() because we are using physics
+    {
+        // Add forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        // Check if user is pressing the "right" key (either right arrow or 'd')
+        if (Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow))
+        {
+            // Add sideways force
+            rb.AddForce(0, jumpForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+        }
+
+        // Check if user is pressing the "right" key (either right arrow or 'd')
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+        {
+            // Add sideways force
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        // Check if user is pressing the "left" key (either left arrow or 'a')
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        {
+            // Add sideways force
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f) {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
